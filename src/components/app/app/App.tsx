7da593195c8befafa65784b73b6fd3c3154790de
Mihail Makeychev <mikhail@ilartech.ru>
2024-12-24 18:11:58 +0300

    fix

--- src/components/app/app/App.tsx
+++ src/components/app/app/App.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { AppRoute } from "../../../const";
 import StartPage from "../../../pages/startPage/startPage";
-import AboutHealth from "../../../pages/aboutHealth/aboutHealth";
-import AboutRepose from "../../../pages/aboutRepose/aboutRepose";
 
 const App = (): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route path={AppRoute.Main} element={<StartPage />} />
-      <Route path={AppRoute.AboutHealth} element={<AboutHealth />} />
-      <Route path={AppRoute.AboutRepose} element={<AboutRepose />} />
     </Routes>
   </BrowserRouter>
 );
--- src/pages/startPage/startPage.tsx
+++ src/pages/startPage/startPage.tsx
@@ -1,20 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "antd";
-import { AppRoute } from "../../const";
 import styles from "./startPage.module.css";
 
 const StartPage = (): JSX.Element => (
   <div className="container">
-    <div className={styles.startPageButtonsWrap}>
-      <Link className={styles.button} to={AppRoute.AboutHealth}>
-        О здравии
-      </Link>
-
-      <Link className={styles.button} to={AppRoute.AboutRepose}>
-        Об упокоении
-      </Link>
-    </div>
+    <div className={styles.startPageButtonsWrap}></div>
   </div>
 );
 
